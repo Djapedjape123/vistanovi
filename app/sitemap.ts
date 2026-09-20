d3a836693ae8fa71f3216197b406368c6dc536d9
Djapedjape123 <petafipedjaradic@gmail.com>
2026-09-20 21:26:04 +0200

    nova sitemapa

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from 'next';
 
-const siteUrl = 'https://vistanovi.com'; 
+const siteUrl = 'https://www.vistanovi.com'; 
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const now = new Date();
